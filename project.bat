bb687f1a00b9be38f716739555dde27082e72d32
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2017-06-12 10:59:48 +0200

    project.bat now finds out DEBUG or RELEASE build depending on current branch

--- project.bat
+++ project.bat
@@ -1,5 +1,17 @@
 @echo off
 
 set PROJECT=Linqy
-set SUFFIX=
-set CONFIGURATION=Release
+
+set GITBRANCH=
+for /f %%f in ('git rev-parse --abbrev-ref HEAD') do set GITBRANCH=%%f
+
+if "%GITBRANCH%" == "master" (
+    set SUFFIX=
+    set CONFIGURATION=Release
+    echo Building RELEASE build
+    exit /B 0
+)
+
+echo Building BETA build
+set SUFFIX=-beta
+set CONFIGURATION=Debug
